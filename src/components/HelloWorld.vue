726970f76b053cc652a7612ac62fc844c010aba9
Arnav Gupta <arnav@codingblocks.com>
2018-12-06 01:03:29 +0530

    add prettier rules for myself
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -111,7 +111,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class HelloWorld extends Vue {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,13 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 })
 export default class Home extends Vue {}
 </script>
