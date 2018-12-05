706e686f239928ab5f958998f0fc2e3258dc6535
Arnav Gupta <arnav@codingblocks.com>
2018-12-06 03:01:10 +0530

    Add all the templates
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <div class="container">
+      <a href="/" class="logo-font">conduit</a>
+      <span class="attribution">
+        An interactive learning project from
+        <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed
+        under MIT.
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class AppFooter extends Vue {}
+</script>
--- src/router.ts
+++ src/router.ts
@@ -12,13 +12,19 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/Register.vue'),
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import('@/views/Settings.vue'),
     },
   ],
 });
